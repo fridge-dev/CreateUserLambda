48fe6783fb1393690789cdf955c149b05746862f
Alec Von Arx <al.vonarx+fridge-dev@gmail.com>
2020-02-16 17:09:55 -0800

    paramaterize username in invoke test script

--- lambda-invoke-test.sh
+++ lambda-invoke-test.sh
@@ -2,11 +2,11 @@ RESPONSE_JSON_FILE=/tmp/invoke_output.txt
 aws lambda invoke \
     --function-name CreateUserHandler \
     --cli-binary-format raw-in-base64-out \
-    --payload '{
-        "username": "fridge-guy",
-        "password": "fake-password-aspo8hgao8ehfa",
-        "userSpec": "SIMPLE/PIN"
-    }' \
+    --payload "{
+        \"username\": \"$1\",
+        \"password\": \"fake-password-aspo8hgao8ehfa\",
+        \"userSpec\": \"SIMPLE/PIN\"
+    }" \
     $RESPONSE_JSON_FILE \
 && echo -e "\nResponse paylod:" \
 && cat $RESPONSE_JSON_FILE | jq \
